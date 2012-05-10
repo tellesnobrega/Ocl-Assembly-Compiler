a5b5cfc2fba6963329ac0eb9cafac0322dd3bf18
tellesnobrega <tellesnobrega@gmail.com>
2012-05-09 22:13:06 -0300

    Atualizações no projeto

--- Ocl-Assembly-Compiler/src/main/Main.java
+++ Ocl-Assembly-Compiler/src/main/Main.java
@@ -1,10 +1,31 @@
 package main;
 
+import gerador.parser.TradutorXMI;
 import java_cup.internal_error;
 
 public class Main {
+	
+	public static void executaAnalisadores(String ocl, String uml) throws Exception {
+		TradutorXMI.getInstance().traduz(uml);
+		Compilador.executaAnalisadorSintatico(ocl);
+	}
 
 	public static void main(String[] args) throws internal_error, Exception {
-		Compilador.executaAnalisadorSintatico(args[0]);
+		
+		if (args.length == 0) {
+			try {
+				String arquivoOcl = "teste.txt";
+				String arquivoXml = "profe_modificado.xml";
+				executaAnalisadores(arquivoOcl, arquivoXml);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				executaAnalisadores(args[0], args[1]);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
